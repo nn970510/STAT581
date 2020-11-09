31ebab6a16380d933b314058e1dd5ba6b3266605
a1code <abhishek123bhatt@gmail.com>
2020-11-09 14:17:30 -0500

    stat581//assignment 10 with correct results

--- Tianzhi_Abhishek_Harshini_Assignment10/AS10.R
+++ Tianzhi_Abhishek_Harshini_Assignment10/AS10.R
@@ -2,6 +2,10 @@ SPRT <- function(alpha, beta, theta0, theta1, input_seq) {
 	n=length(input_seq)	# stores number of random variables so far
 	sum_input_seq = sum(input_seq)
 
+	print('input sequence:')
+	print(input_seq)
+	print(paste('n', n))
+
 	a = log(beta / (1-alpha))
 	b = log((1-beta) / alpha)
 	print(paste('lower bound a', a))
@@ -21,7 +25,6 @@ SPRT <- function(alpha, beta, theta0, theta1, input_seq) {
 
 sim <- function(p){
 	outcome_seq<-c()
-	seq<-c()
 	theta0<-0.45
 	theta1<-0.55
 	alpha<-0.01
@@ -31,22 +34,19 @@ sim <- function(p){
 	h0<-'p<=0.45'
 	h1<-'p>=0.55'
 
-	theta0<-p
-
 	while(stop == 0)
 	{
 		outcome_seq = append(outcome_seq, rbinom(1,1,p))
-		seq = append(seq, sum(outcome_seq))
-		out = SPRT(alpha, beta, theta0, theta1, seq)
-		print(out[3])
+
+		out = SPRT(alpha, beta, theta0, theta1, outcome_seq)
 		stop<-out[3]
 	}
 
-	ifelse((out[3] == 1), print(paste("accept: ", h0, "reject: ", h1)), 
+	ifelse((out[3] == -1), print(paste("accept: ", h0, "reject: ", h1)), 
 		print(paste("accept: ", h1, "reject: ", h0)))
 	print('-----------------------------------------------------------')
 }
 
 sim(0.3)
-sim(0.56)
-sim(0.54)
+# sim(0.56)
+# sim(0.54)
